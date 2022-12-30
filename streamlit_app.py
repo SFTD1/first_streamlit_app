f81bda215ae46d0b23fa5cec4989317215723df3
SnowFTD <david.zehnder01@gmail.com>
2022-12-30 09:43:36 +0100

    Update streamlit_app.py
    bug fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,7 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # Let's put a pick list here so they can pick the fruit they want to include 
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
-fruits_to_show = my_fruit_list.loc[fruits_selected]he screen
+fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
@@ -30,5 +30,5 @@ streamlit.text(fruityvice_response.json()) # just write the data to the screen
 
 # aus json wird tabelle
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# display the data on t
+# display the data on the screen
 streamlit.dataframe(fruityvice_normalized)
